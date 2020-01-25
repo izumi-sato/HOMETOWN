00675218be4d38e7c4aeac3e5db71bde385d0134
EC2 Default User <ec2-user@ip-172-31-88-34.ec2.internal>
2020-01-25 13:31:18 +0000

    ログイン別表示トップ画面

--- app/controllers/ivents_controller.rb
+++ app/controllers/ivents_controller.rb
@@ -1,9 +1,24 @@
 class IventsController < ApplicationController
+    before_action :move_to_index, except: :index
+  
   def index
     @ivents=Ivent.all
   end
   
   def new
+  end
+  
+  def create
+    Ivent.create(ivent_params)
+  end
+
+  private
+  def ivent_params
+    params.permit(:ivent_name, :text, :owner_name, :start_day, :end_day,:place, :url, :image)
+  end
+  
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
   
 end
